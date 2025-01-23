5c26e083612e842b98e57e2af83191c546f84762
gnl00 <gnl0@qq.com>
2025-01-23 18:47:08 +0800

    update: feature & ui

--- src/renderer/src/components/chat/ChatItemComponent.tsx
+++ src/renderer/src/components/chat/ChatItemComponent.tsx
@@ -61,7 +61,7 @@ export const UserChatItemRef: React.FC<UserChatItemProps> = forwardRef<HTMLDivEl
           {
             idx === msgSize && !lastMsgStatus && <span className="flex items-end pr-1 text-orange-500 font-bold text-lg"><i onClick={e => reGenerate(message.body.content)} className="ri-refresh-line"></i></span>
           }
-            <div className={cn("max-w-[85%] rounded-2xl px-4 py-3 shadow-lg bg-[#eff6ff] dark:bg-blue-200")}>
+            <div className={cn("max-w-[85%] rounded-2xl px-4 py-3 shadow-lg bg-gray-700 dark:bg-gray-800")}>
               {typeof message.body.content !== 'string' ? (
                 <>
                   <div className="space-y-1">
@@ -108,7 +108,7 @@ export const UserChatItemRef: React.FC<UserChatItemProps> = forwardRef<HTMLDivEl
                   remarkPlugins={[remarkGfm]}
                   rehypePlugins={[rehypeRaw]}
                   remarkRehypeOptions={{ passThrough: ['link'] }}
-                  className={cn("prose prose-code:text-gray-400 text-md font-medium max-w-[100%] text-[#3b82f6] transition-all duration-400 ease-in-out")}
+                  className={cn("prose prose-code:text-gray-400 text-md font-medium max-w-[100%] text-gray-200 transition-all duration-400 ease-in-out")}
                   components={{
                     code(props) {
                       const { children, className, node, ...rest } = props
@@ -171,12 +171,12 @@ export const AssistantChatItemRef: React.FC<AssistantChatItemProps> = forwardRef
     <ContextMenu key={idx} modal={true}>
       <ContextMenuTrigger asChild>
         <div ref={elRef} key={idx} className="flex justify-start mb-2">
-        <div className="max-w-[85%] rounded-2xl bg-gray-100 px-4 py-3 text-gray-900 shadow-lg dark:bg-gray-400 dark:text-slate-50 overflow-y-scroll">
+        <div className="max-w-[85%] rounded-2xl px-4 py-3 shadow-lg  bg-gray-50 dark:bg-gray-600 overflow-y-scroll">
               <ReactMarkdown
                 remarkPlugins={[remarkGfm]}
                 rehypePlugins={[rehypeRaw]}
                 remarkRehypeOptions={{ passThrough: ['link'] }}
-                className="prose prose-code:text-gray-400 dark:prose-code:text-gray-500 text-md font-medium max-w-[100%] transition-all duration-400 ease-in-out"
+                className="prose prose-code:text-gray-400 dark:prose-code:text-gray-100 prose-counter:text-inherient  dark:text-slate-300 text-md font-medium max-w-[100%] transition-all duration-400 ease-in-out"
                 components={{
                   code(props) {
                     const { children, className, node, ...rest } = props
--- src/renderer/src/components/chat/ChatListComponent.tsx
+++ src/renderer/src/components/chat/ChatListComponent.tsx
@@ -5,7 +5,8 @@ import { debounce } from 'lodash'
 import AnimateHeight, { Height } from 'react-animate-height'
 import { Transition } from 'react-transition-group'
 import { useChatStore } from "@renderer/store"
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll'
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { useChatContext } from "@renderer/context/ChatContext"
 
 interface ChatListProps {
     chatWindowHeight?: number
@@ -14,93 +15,95 @@ interface ChatListProps {
 
 const ChatListComponent = (props: ChatListProps) => {
     const { chatWindowHeight, regenChat } = props
+    const [localFetchState, setLocalFetchState] = useState<boolean>()
     const [editableContentId, setEditableContentId] = useState(-1)
-    const { messages } = useChatStore()
+    const { messages, fetchState } = useChatStore()
+    const { chatId } = useChatContext()
     const chatListRef = useRef<VListHandle>(null)
     const scrollEndRef = useRef<HTMLDivElement>(null)
     const chatListRefs = useMemo(() => {
         return Array(messages.length).fill(null).map(() => React.createRef<HTMLDivElement>())
     }, [messages])
     useEffect(() => {
-        // scrollEndRef.current?.scrollIntoView({
-        //     behavior: 'instant'
-        // })
-        // const debouncedScrollToIndex = debounce(() => {
-        //     chatListRef.current?.scrollToIndex(messages.length - 1, {
-        //         align: 'start',
-        //         smooth: true
-        //     })
-        // }, 99)
-        // debouncedScrollToIndex()
-        // return () => {
-        //     debouncedScrollToIndex.cancel();
-        // }
-    }, [messages])
+        chatListRef.current?.scrollToIndex(messages.length, {
+            align: 'end',
+            smooth: true
+        })
+    }, [chatId])
+    // useEffect(() => {
+    //     if(fetchState != localFetchState) {
+    //         chatListRef.current?.scrollToIndex(messages.length, {
+    //             align: 'end',
+    //             smooth: true
+    //         })
+    //     }
+    //     setLocalFetchState(fetchState)
+    // }, [fetchState])
     return (
-        <div className="h-full space-x-2 w-[100vw] overflow-y-auto" id="chatContainer">
-            {
-                messages.map((message, index) => {
-                    if (!message.body || !message.body.content || message.body.content.length === 0) {
-                        return null
-                    }
-                    const elementName = `message-${index}`
-                    return (
-                        <Element name={elementName} key={index} className="scroll-smooth">
-                            {message.body.role === 'user' ? (
-                                <UserChatItemRef
-                                    idx={index}
-                                    elRef={chatListRefs[index]}
-                                    message={message}
-                                    msgSize={messages.length}
-                                    reGenerate={(textCtx, mediaCtx) => regenChat(textCtx, mediaCtx)}
-                                />
-                            ) : (
-                                <AssistantChatItemRef
-                                    idx={index}
-                                    elRef={chatListRefs[index]}
-                                    msgSize={messages.length}
-                                    message={message}
-                                    editableContentId={editableContentId}
-                                    setEditableContentId={setEditableContentId}
-                                />
-                            )}
-                        </Element>
-                    )
-                })
-            }
-            <Element name="scrollEnd" className="scrollEndRef" ref={scrollEndRef} />
-        </div>
-
-        // <VList ref={chatListRef} className="space-y-2" style={{ height: chatWindowHeight ? chatWindowHeight - 12 : 900, scrollBehavior: 'smooth' }}>
+        // <div className="h-full space-x-2 w-[100vw] overflow-y-auto" id="chatContainer">
         //     {
         //         messages.map((message, index) => {
         //             if (!message.body || !message.body.content || message.body.content.length === 0) {
-        //                 return
+        //                 return null
         //             }
-        //             return message.body.role == 'user' ?
-        //                 <UserChatItemRef
-        //                     key={index}
-        //                     idx={index}
-        //                     elRef={chatListRefs[index]}
-        //                     message={message}
-        //                     msgSize={messages.length}
-        //                     reGenerate={(textCtx, mediaCtx) => regenChat(textCtx, mediaCtx)}
-        //                 />
-        //                 :
-        //                 <AssistantChatItemRef
-        //                     key={index}
-        //                     idx={index}
-        //                     elRef={chatListRefs[index]}
-        //                     msgSize={messages.length}
-        //                     message={message}
-        //                     editableContentId={editableContentId}
-        //                     setEditableContentId={setEditableContentId}
-        //                 />
+        //             const elementName = `message-${index}`
+        //             return (
+        //                 <Element name={elementName} key={index} className="scroll-smooth">
+        //                     {message.body.role === 'user' ? (
+        //                         <UserChatItemRef
+        //                             idx={index}
+        //                             elRef={chatListRefs[index]}
+        //                             message={message}
+        //                             msgSize={messages.length}
+        //                             reGenerate={(textCtx, mediaCtx) => regenChat(textCtx, mediaCtx)}
+        //                         />
+        //                     ) : (
+        //                         <AssistantChatItemRef
+        //                             idx={index}
+        //                             elRef={chatListRefs[index]}
+        //                             msgSize={messages.length}
+        //                             message={message}
+        //                             editableContentId={editableContentId}
+        //                             setEditableContentId={setEditableContentId}
+        //                         />
+        //                     )}
+        //                 </Element>
+        //             )
         //         })
         //     }
-        //     <div ref={scrollEndRef} className="scrollEndRef"></div>
-        // </VList>
+        //     <div id="scrollEnd" className="scrollEndRef" ref={scrollEndRef} />
+        // </div>
+
+        <VList ref={chatListRef} className="space-y-2" style={{ height: chatWindowHeight ? chatWindowHeight - 12 : 900, scrollBehavior: 'smooth' }}>
+            {
+                messages.map((message, index) => {
+                    if (!message.body || !message.body.content || message.body.content.length === 0) {
+                        return
+                    }
+                    return message.body.role == 'user' ?
+                        <UserChatItemRef
+                            key={index}
+                            idx={index}
+                            elRef={chatListRefs[index]}
+                            message={message}
+                            msgSize={messages.length}
+                            reGenerate={(textCtx, mediaCtx) => regenChat(textCtx, mediaCtx)}
+                        />
+                        :
+                        <AssistantChatItemRef
+                            key={index}
+                            idx={index}
+                            elRef={chatListRefs[index]}
+                            msgSize={messages.length}
+                            message={message}
+                            editableContentId={editableContentId}
+                            setEditableContentId={setEditableContentId}
+                        />
+                })
+            }
+            <div ref={scrollEndRef} className="scrollEndRef"></div>
+        </VList>
     )
 }
 
-export default React.memo(ChatListComponent)
+export default ChatListComponent
