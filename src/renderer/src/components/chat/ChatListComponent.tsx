242ccdf1fc2ce10ba0fd8c083f9246b07474c1fc
gnl00 <gnl0@qq.com>
2025-02-05 18:24:07 +0800

    update: feature & ui

--- src/renderer/src/components/chat/ChatListComponent.tsx
+++ src/renderer/src/components/chat/ChatListComponent.tsx
@@ -63,7 +63,11 @@ const ChatListComponent = (props: ChatListProps) => {
                     )
                 })
             }
-            <div className="absolute bg-black/30 backdrop-blur-lg text-gray-300 z-10 right-1 bottom-1 rounded-full w-8 h-8 p-1 flex items-center justify-center" onClick={onEndClick}><i className="ri-arrow-down-fill"></i></div>
+            {
+                chatId && (
+                    <div className="absolute bg-black/30 backdrop-blur-lg text-gray-300 z-10 right-4 bottom-1 rounded-full w-8 h-8 p-1 flex items-center justify-center" onClick={onEndClick}><i className="ri-arrow-down-fill"></i></div>
+                )
+            }
             <div id="scrollEnd" className="scrollEndRef" ref={scrollEndRef} />
         </div>
     )
