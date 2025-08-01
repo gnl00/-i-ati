6326f6c4745580b8506f086976eb0d6188efe71a
gnl00 <gnl0@qq.com>
2025-08-01 19:01:02 +0800

    fix: fix provider panel

--- src/renderer/src/components/sys/PreferenceComponent.tsx
+++ src/renderer/src/components/sys/PreferenceComponent.tsx
@@ -19,12 +19,25 @@ import {
     SelectTrigger,
     SelectValue,
     } from "@renderer/components/ui/select"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+    } from "@renderer/components/ui/dropdown-menu"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+    TooltipProvider
+    } from "@renderer/components/ui/tooltip"
 import { Button } from "@renderer/components/ui/button"
 import { Badge } from '@renderer/components/ui/badge'
 import { Label } from '@renderer/components/ui/label'
 import { Popover, PopoverContent, PopoverTrigger } from '@renderer/components/ui/popover'
 import { Command, CommandItem, CommandGroup, CommandEmpty, CommandList, CommandInput } from '@renderer/components/ui/command'
-import { Check, ChevronsUpDown } from "lucide-react"
+import { Check, ChevronsUpDown, SquarePen } from "lucide-react"
 import { Drawer, DrawerHeader, DrawerContent, DrawerTitle, DrawerTrigger, DrawerFooter, DrawerClose } from '@renderer/components/ui/drawer'
 import { Input } from '@renderer/components/ui/input'
 import { toast as sonnerToast } from 'sonner'
@@ -228,6 +241,39 @@ const PreferenceComponent: React.FC<PreferenceProps> = () => {
         }
         return <img draggable={false} src={iconSrc} alt="OpenAI" className="w-14 h-14" />
     }
+    const getIconSrc = (provider: string) => {
+        let iconSrc = robotIcon
+        const pName = provider.toLowerCase()
+        switch (pName) {
+            case "OpenAI".toLowerCase():
+                iconSrc = openaiIcon
+                break
+            case "Anthropic".toLowerCase():
+                iconSrc = anthropicIcon
+                break
+            case "DeepSeek".toLowerCase():
+                iconSrc = deepseekIcon
+                break
+            case "MoonShot".toLowerCase():
+                iconSrc = moonshotIcon
+                break
+            case "SilliconFlow".toLowerCase() || "SiliconCloud".toLowerCase():
+                iconSrc = siliconcloudIcon
+                break
+            case "OpenRouter".toLowerCase():
+                iconSrc = openrouterIcon
+                break
+            case "Ollamma".toLowerCase():
+                iconSrc = ollamaIcon
+                break
+            case "Groq".toLowerCase():
+                iconSrc = groqIcon
+                break
+            default:
+                break
+        }
+        return iconSrc
+    }
     const onModelTableCellClick = (val: string) => {
         navigator.clipboard.writeText(val)
         sonnerToast(`✅ Copied`)
@@ -291,10 +337,194 @@ const PreferenceComponent: React.FC<PreferenceProps> = () => {
             </div>
             <Tabs defaultValue="providers">
                 <TabsList>
-                    <TabsTrigger value="providers">Providers</TabsTrigger>
+                    <TabsTrigger value="provider-list">Providers</TabsTrigger>
                     <TabsTrigger value="tool">Tool</TabsTrigger>
                     <TabsTrigger value="mcp-server">MCP Server</TabsTrigger>
                 </TabsList>
+                <TabsContent value="provider-list" className='w-[670px] h-[600px] focus:ring-0 focus-visible:ring-0'>
+                    <div className='flex h-full bg-gray-50 p-1 rounded-md'>
+                        <div className='w-1/4 flex flex-col overflow-scroll scroll-smooth relative'>
+                        <div className={cn('bg-gray-100 rounded-md')}>
+                            <Drawer>
+                                <DrawerTrigger className='text-gray-400 flex justify-center items-center bg-gray-100 rounded-xl h-full w-full'>
+                                    <p id='add-new-provider' className='sticky bottom-0 w-full h-14 bg-gray-50 rounded-md flex justify-center items-center'>
+                                        <span className='bg-gray-200 w-full h-full flex justify-center items-center mx-1 my-1 rounded-md text-gray-600 text-2xl hover:bg-black/5 hover:text-gray-400'><i className="ri-add-circle-line"></i></span>
+                                    </p>
+                                </DrawerTrigger>
+                                <DrawerContent>
+                                    <DrawerHeader>
+                                        <DrawerTitle>Add new provider</DrawerTitle>
+                                    </DrawerHeader>
+                                    <DrawerFooter>
+                                        <div id='add-new-provider-drawer' className="grid gap-4 app-undragable">
+                                            <div className="flex flex-col gap-2">
+                                                <div className="flex flex-col items-start gap-2">
+                                                    <Label htmlFor="name">Name</Label>
+                                                    <Input
+                                                        id="name"
+                                                        placeholder="OpenAI"
+                                                        className="focus-visible:ring-transparent focus-visible:ring-offset-0 w-full h-10"
+                                                        onChange={e => { setNewProviderName(e.target.value) }}
+                                                    />
+                                                </div>
+                                                <div className="flex flex-col items-start gap-2">
+                                                    <Label htmlFor="apiUrl">API URL</Label>
+                                                    <Input
+                                                        id="apiUrl"
+                                                        placeholder="https://api.openai.com/v1/chat/completions"
+                                                        className="focus-visible:ring-transparent focus-visible:ring-offset-0 w-full h-10"
+                                                        onChange={e => { setNewProviderApi(e.target.value) }}
+                                                    />
+                                                </div>
+                                                <div className="flex flex-col items-start gap-2">
+                                                    <Label htmlFor="apiKey">API Key</Label>
+                                                    <Input
+                                                        id="apiKey"
+                                                        placeholder="sk-********"
+                                                        className="focus-visible:ring-transparent focus-visible:ring-offset-0 w-full h-10"
+                                                        onChange={e => { setNewProviderApiKey(e.target.value) }}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <DrawerTrigger asChild>
+                                            <Button onClick={onAddProviderBtnClick}>Save</Button>
+                                        </DrawerTrigger>
+                                        <DrawerClose asChild>
+                                            <Button variant="outline">Cancel</Button>
+                                        </DrawerClose>
+                                    </DrawerFooter>
+                                </DrawerContent>
+                            </Drawer>
+                        </div>
+                        {
+                            providers.map((p, idx) => (
+                                <div key={idx} className='flex flex-col pl-1 py-0.5 justify-center items-center select-none' onMouseEnter={_ => onProviderCardHover(idx)} onMouseLeave={_ => onProviderCardHover(-1)} onClick={_ => onProviderCardClick(p)}>
+                                    <p className='flex items-center bg-gray-100 hover:bg-blue-gray-200 rounded-md w-full py-2 px-1 text-gray-700 space-x-2 relative'>
+                                        <img draggable={false} src={getIconSrc(p.name)} alt="OpenAI" className="w-5 h-5" />
+                                        <span>{p.name}</span>
+                                        {
+                                            hoverProviderCardIdx === idx && (
+                                                <div className='absolute top-0 right-0' onClick={_ => onProviderCardDelClick(idx)}>
+                                                    <Cross1Icon className="rounded-full bg-red-500 text-white p-0.5 w-4 h-4 transition-all duration-300 ease-in-out hover:transform hover:rotate-180" />
+                                                </div>
+                                            )
+                                        }
+                                    </p>
+                                </div>
+                            ))
+                        }
+                        </div>
+                        <div className='w-3/4 px-1 pt-1 flex flex-col'>
+                            <div className='flex flex-col space-y-1 mb-1'>
+                                <Label>
+                                    <p className='pl-1 pb-0.5 text-gray-400'>Provider Name</p>
+                                    <Input className='w-full' placeholder='provider name' value={editProviderName} 
+                                        onChange={(e) => {
+                                            setEditProviderName(e.target.value)
+                                            setCurrentProviderName(e.target.value)
+                                            updateCurrentProvider('name', e.target.value)
+                                        }}
+                                    />
+                                </Label>
+                                <Label>
+                                    <p className='pl-1 pb-0.5 text-gray-400'>ApiUrl</p>
+                                    <Input className='w-full' placeholder='api url' value={editProviderApiUrl}
+                                        onChange={(e) => {
+                                            setEditProviderApiUrl(e.target.value)
+                                            updateCurrentProvider('apiUrl', e.target.value)
+                                        }}
+                                    />
+                                </Label>
+                                <Label>
+                                    <p className='pl-1 pb-0.5 text-gray-400'>ApiKey</p>
+                                    <Input className='w-full' placeholder='api key' value={editProviderApiKey}
+                                        onChange={(e) => {
+                                            setEditProviderApiKey(e.target.value)
+                                            updateCurrentProvider('apiKey', e.target.value)
+                                        }}
+                                    />
+                                </Label>
+                            </div>
+                            <Table id="provider-models-table" className='relative'>
+                                <TableHeader className='text-sm select-none sticky top-0 bg-gray-100 z-10'>
+                                    <TableRow>
+                                        <TableHead className='text-center flex items-center space-x-1'><Checkbox onCheckedChange={toggleEnableAllModels} /><span>Enable</span></TableHead>
+                                        <TableHead className='text-center'>DisplayName</TableHead>
+                                        <TableHead className='text-center'>ModelValue</TableHead>
+                                        <TableHead className='text-center'>Type</TableHead>
+                                        <TableHead className='text-center'>Operation</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell><Checkbox checked={nextAddModelEnable} onCheckedChange={onNextAddModelEnableChange} /></TableCell>
+                                        <TableCell><Input className='focus-visible:ring-transparent focus-visible:ring-offset-0 h-8' value={nextAddModelLabel} onChange={e => setNextAddModelLabel(e.target.value)} /></TableCell>
+                                        <TableCell><Input className='focus-visible:ring-transparent focus-visible:ring-offset-0 h-8' value={nextAddModelValue} onChange={e => setNextAddModelValue(e.target.value)} /></TableCell>
+                                        <TableCell>
+                                            <Select value={nextAddModelType} onValueChange={setNextAddModelType}>
+                                                <SelectTrigger className="h-8">
+                                                    <SelectValue placeholder="Type" />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    <SelectGroup>
+                                                        <SelectItem value="llm">LLM</SelectItem>
+                                                        <SelectItem value="vlm">VLM</SelectItem>
+                                                    </SelectGroup>
+                                                </SelectContent>
+                                            </Select>
+                                        </TableCell>
+                                        <TableCell className='text-center'><Button onClick={onAddModelClick} size={'xs'} variant={'outline'}><i className="ri-add-circle-line text-lg"></i></Button></TableCell>
+                                    </TableRow>
+                                    {
+                                        providers.find(p => p.name === currentProviderName)?.models.map((m, idx) => (
+                                            <TableRow key={idx} className={cn(idx % 2 === 0 ? 'bg-blue-gray-100' : 'bg-blue-gray-50')}>
+                                                <TableCell><Checkbox checked={m.enable} onCheckedChange={checked => onModelEnableStatusChange(checked, m)}/></TableCell>
+                                                <TableCell className='text-left max-w-0' onClick={_ => onModelTableCellClick(m.name)}>
+                                                    <TooltipProvider>
+                                                        <Tooltip>
+                                                            <TooltipTrigger asChild>
+                                                                <p className='truncate'>{m.name}</p>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent>
+                                                                <p>{m.name}</p>
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    </TooltipProvider>
+                                                </TableCell>
+                                                <TableCell className='text-left max-w-0' onClick={_ => onModelTableCellClick(m.value)}>
+                                                <TooltipProvider>
+                                                        <Tooltip>
+                                                            <TooltipTrigger asChild>
+                                                                <p className='truncate'>{m.value}</p>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent>
+                                                                <p>{m.value}</p>
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    </TooltipProvider>
+                                                </TableCell>
+                                                <TableCell className='text-center'>{m.type}</TableCell>
+                                                <TableCell className='text-center'>
+                                                    <DropdownMenu>
+                                                        <DropdownMenuTrigger asChild>
+                                                            <Button variant="outline" size='xs' className='rounded-lg'><SquarePen className='w-3'></SquarePen></Button>
+                                                        </DropdownMenuTrigger>
+                                                        <DropdownMenuContent className="w-auto" align="start">
+                                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                                            <DropdownMenuSeparator />
+                                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                                        </DropdownMenuContent>
+                                                    </DropdownMenu>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </div>
+                </TabsContent>
                 <TabsContent value="providers" className='w-[640px] h-[600px] focus:ring-0 focus-visible:ring-0'>
                     <div ref={providerCardRef} className="m-2 h-20 overflow-scroll scroll-smooth no-scrollbar">
                         <div className={cn('select-none h-20 flex space-x-2 relative', providers.length === 0 ? 'flex justify-center' : '')}>
